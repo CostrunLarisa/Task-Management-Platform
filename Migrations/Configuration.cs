b63ce33a899d3abce1649e419c1f0223f2aee584
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2020-11-06 20:51:36 +0200

    entity-framework

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_Management_Platform.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Task_Management_Platform.Models.AppData>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "Task_Management_Platform.AppData";
+        }
+
+        protected override void Seed(Task_Management_Platform.Models.AppData context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Models/AppData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Task_Management_Platform.Models
+{
+    public class AppData : DbContext
+    {
+            public AppData(): base ("DBConnectionString") 
+            {
+                Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppData, Task_Management_Platform.Migrations.Configuration>("DBConnectionString"));
+            }
+    }
+}
